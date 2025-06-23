68d2efa8550ebf4dfcf3b198aaa70f310c26deaa
stephsallum <steph.sallum@gmail.com>
2025-06-23 10:42:46 -0700

    quicklook script clean up

--- scalesdrp/scripts/scales_quicklook.py
+++ scalesdrp/scripts/scales_quicklook.py
@@ -30,7 +30,6 @@ import logging.config
 def _parse_arguments(in_args: list) -> argparse.Namespace:
     description = "SCALES pipeline CLI"
 
-    # this is a simple case where we provide a frame and a configuration file
     parser = argparse.ArgumentParser(prog=f"{in_args[0]}",
                                      description=description)
     parser.add_argument('-c', '--config', dest="SCALES_config_file", type=str,
@@ -44,30 +43,6 @@ def _parse_arguments(in_args: list) -> argparse.Namespace:
     parser.add_argument('-l', '--list', dest='file_list',
                         help='File containing a list of files to be processed',
                         default=None)
-    parser.add_argument('-g', '--groups', dest='group_mode',
-                        help='Use group mode: separate files by image type and '
-                             'reduce in the correct order',
-                        default=False, action='store_true')
-    parser.add_argument('-t', '--taperfrac', dest='taperfrac', type=float,
-                        help='Taper fraction for wavelength fitting',
-                        default=None)
-    parser.add_argument('-a', '--atlas_line_list', dest='atlas_line_list',
-                        type=str, help="Atlas line list file", default=None)
-    parser.add_argument('-M', '--middle_fraction', dest='middle_fraction',
-                        type=float, help="Fraction of middle to use",
-                        default=None)
-    parser.add_argument('-o', '--atlas_offset', dest='atlas_offset',
-                        type=int, help="Atlas offset (px)", default=None)
-    parser.add_argument('-e', '--line_thresh', dest='line_thresh',
-                        type=float, help="Line Cleaning Threshold (e-)",
-                        default=None)
-    parser.add_argument('-u', '--tukey_alpha', dest='tukey_alpha',
-                        type=float, help="Tukey Window Alpha (0.0 - 1.0)",
-                        default=None)
-    parser.add_argument('-F', '--max_frac', dest='max_frac',
-                        type=float, default=None,
-                        help="Fraction of line max for fitting window "
-                             "(default: 0.5)")
 
     # in this case, we are loading an entire directory,
     # and ingesting all the files in that directory
@@ -128,7 +103,7 @@ def main():
     if args.write_config:
         dest = os.path.join(os.getcwd(), 'scales_quicklook.cfg')
         if os.path.exists(dest):
-            print("Config file scales.cfg already exists in current dir")
+            print("Config file scales_quicklook.cfg already exists in current dir")
         else:
             scales_config_file = 'configs/scales_quicklook.cfg'
             scales_config_fullpath = pkg_resources.resource_filename(
@@ -136,30 +111,6 @@ def main():
             shutil.copy(scales_config_fullpath, os.getcwd())
             print("Copied scales_quicklook.cfg into current dir.  Edit and use with -c")
         sys.exit(0)
-
-    # This check can be removed once reduce_scales processes are
-    # siloed against each other
-    plist = []
-    for p in psutil.process_iter():
-        try:
-            if "scales_quicklook" in p.name():
-                plist.append(p)
-        except psutil.NoSuchProcess:
-            continue
-    # plist = [p for p in psutil.process_iter() if "reduce_scales" in p.name()]
-    if len(plist) > 1:
-        print("DRP already running in another process, exiting")
-        sys.exit(0)
-
-    def process_subset(in_subset):
-        for in_frame in in_subset.index:
-            arguments = Arguments(name=in_frame)
-            framework.append_event('next_file', arguments, recurrent=True)
-    
-    def process_list(in_list):
-        for in_frame in in_list:
-            arguments = Arguments(name=in_frame)
-            framework.append_event('next_file', arguments, recurrent=True)
 
     # make sure user has selected a channel
     if not args.lowres and not args.medres:
@@ -232,85 +183,6 @@ def main():
             framework.context.pipeline_logger.info("Skipping sky subtraction")
             framework.config.instrument.skipsky = args.skipsky
 
-    # check for taperfrac argument
-    if args.taperfrac:
-        def_tf = getattr(framework.config.instrument, 'TAPERFRAC', None)
-        if def_tf is not None:
-            framework.context.pipeline_logger.info(
-                "Setting new taperfrac = %.3f" % args.taperfrac)
-            framework.config.instrument.TAPERFRAC = args.taperfrac
-
-    # check for middle_fraction argument
-    if args.middle_fraction:
-        def_mf = getattr(framework.config.instrument, 'MIDFRAC', None)
-        if def_mf is not None:
-            framework.context.pipeline_logger.info(
-                "Setting new middle_fraction = %.2f" % args.middle_fraction)
-            framework.config.instrument.MIDFRAC = args.middle_fraction
-
-    # check for atlas_offset argument
-    if args.atlas_offset:
-        def_ao = getattr(framework.config.instrument, 'ATOFF', None)
-        if def_ao is not None:
-            framework.context.pipeline_logger.info(
-                "Setting new atlas offset = %.2f" % args.atlas_offset)
-            framework.config.instrument.ATOFF = args.atlas_offset
-
-    # check for line_thresh argument
-    if args.line_thresh:
-        def_lt = getattr(framework.config.instrument, 'LINETHRESH', None)
-        if def_lt is not None:
-            framework.context.pipeline_logger.info(
-                "Setting new line thresh = %.2f" % args.line_thresh)
-            framework.config.instrument.LINETHRESH = args.line_thresh
-    else:
-        if args.lowres:
-            framework.config.instrument.LINETHRESH = float(
-                scales_config.LOWRES['linethresh'])
-        elif args.medres:
-            framework.config.instrument.LINETHRESH = float(
-                scales_config.MEDRES['linethresh'])
-
-    # check for tukey_alpha argument
-    if args.tukey_alpha:
-        def_ta = getattr(framework.config.instrument, 'TUKEYALPHA', None)
-        if def_ta is not None:
-            framework.context.pipeline_logger.info(
-                "Setting new tukey alpha = %.2f" % args.tukey_alpha)
-            framework.config.instrument.TUKEYALPHA = args.tukey_alpha
-    else:
-        if args.lowres:
-            framework.config.instrument.TUKEYALPHA = float(
-                scales_config.LOWRES['tukeyalpha'])
-        elif args.medres:
-            framework.config.instrument.TUKEYALPHA = float(
-                scales_config.MEDRES['tukeyalpha'])
-
-    # check for max_frac argument
-    if args.max_frac:
-        def_fm = getattr(framework.config.instrument, 'FRACMAX', None)
-        if def_fm is not None:
-            framework.context.pipeline_logger.info(
-                "Setting new line windowing max fraction = %.2f" %
-                args.max_frac)
-            framework.config.instrument.FRACMAX = args.max_frac
-    else:
-        if args.lowres:
-            framework.config.instrument.FRACMAX = float(
-                scales_config.LOWRES['fracmax'])
-        elif args.medres:
-            framework.config.instrument.FRACMAX = float(
-                scales_config.MEDRES['fracmax'])
-
-    # check for atlas line list argument
-    if args.atlas_line_list:
-        def_ll = getattr(framework.config.instrument, 'LINELIST', None)
-        if def_ll is not None:
-            framework.context.pipeline_logger.info(
-                "Using line list %s instead of generated list" %
-                args.atlas_line_list)
-            framework.config.instrument.LINELIST = args.atlas_line_list
-
     # update proc table argument
     if args.proctab:
         framework.context.pipeline_logger.info(
@@ -328,69 +200,11 @@ def main():
             "Using proc table file %s" % proctab)
         framework.config.instrument.procfile = proctab
 
-    # set up channel specific parameters
-    if args.lowres:
-        framework.config.instrument.arc_min_nframes = int(
-            scales_config.LOWRES['arc_min_nframes'])
-        framework.config.instrument.contbars_min_nframes = int(
-            scales_config.LOWRES['contbars_min_nframes'])
-        framework.config.instrument.object_min_nframes = int(
-            scales_config.LOWRES['object_min_nframes'])
-        framework.config.instrument.minoscanpix = int(
-            scales_config.LOWRES['minoscanpix'])
-        framework.config.instrument.oscanbuf = int(
-            scales_config.LOWRES['oscanbuf'])
-    elif args.medres:
-        framework.config.instrument.arc_min_nframes = int(
-            scales_config.MEDRES['arc_min_nframes'])
-        framework.config.instrument.contbars_min_nframes = int(
-            scales_config.MEDRES['contbars_min_nframes'])
-        framework.config.instrument.object_min_nframes = int(
-            scales_config.MEDRES['object_min_nframes'])
-        framework.config.instrument.minoscanpix = int(
-            scales_config.MEDRES['minoscanpix'])
-        framework.config.instrument.oscanbuf = int(
-            scales_config.MEDRES['oscanbuf'])
-    
-    framework.config.instrument.arc_min_nframes = \
-	scales_config.arc_min_nframes
-    framework.config.instrument.contbars_min_nframes = \
-	scales_config.contbars_min_nframes = \
-    framework.config.instrument.object_min_nframes = \
-	scales_config.object_min_nframes
-    framework.config.instrument.minoscanpix = scales_config.minoscanpix
-    framework.config.instrument.oscanbuf = scales_config.oscanbuf
-
-    # start the bokeh server is requested by the configuration parameters
-    if framework.config.instrument.enable_bokeh is True:
-        if check_running_process(process='bokeh') is False:
-            with open("bokeh_output.txt", "wb") as out:
-                subprocess.Popen('bokeh serve', shell=True, stderr=out,
-                                 stdout=out)
-            # --session-ids=unsigned --session-token-expiration=86400',
-            # shell=True)
-            time.sleep(5)
-        # subprocess.Popen('open http://localhost:5006?bokeh-session-id=scales',
-        # shell=True)
-
     # initialize the proctab and read it
     framework.context.proctab = Proctab()
     framework.context.proctab.read_proctab(framework.config.instrument.procfile)
 
     framework.logger.info("Framework initialized")
-
-    # add a start_bokeh event to the processing queue,
-    # if requested by the configuration parameters
-    if framework.config.instrument.enable_bokeh:
-        framework.append_event('start_bokeh', None)
-
-    # important: to be able to use the grouping mode, we need to reset
-    # the default ingestion action to no-event,
-    # otherwise the system will automatically trigger the next_file event
-    # which initiates the processing
-    if args.group_mode is True:
-        # set the default ingestion event to None
-        framework.config.default_ingestion_event = "add_only"
 
     # start queue manager only (useful for RPC)
     if args.queue_manager_only:
@@ -398,97 +212,12 @@ def main():
         framework.logger.info("Starting queue manager only, no processing")
         framework.start(args.queue_manager_only)
 
-    # in the next two ingest_data command, if we are using standard mode,
-    # the first event generated is next_file.
-    # if we are in groups mode (aka smart mode), then the first event generated
-    # is no_event then, manually, a next_file event is generated for each group
-    # specified in the variable imtypes
-
-    # single frame processing
-    elif args.frames:
-        frames = []
-        for frame in args.frames:
-            # Verify we have the correct channel selected
-            if args.lowres and 'mr' in frame:
-                print('low-res channel requested, but medium-res files in list')
-                qstr = input('Proceed? <cr>=yes or Q=quit: ')
-                if 'Q' in qstr.upper():
-                    frames = []
-                    break
-            if args.medres and 'lr' in frame:
-                print('med-res channel requested, but low-res files in list')
-                qstr = input('Proceed? <cr>=yes or Q=quit: ')
-                if 'Q' in qstr.upper():
-                    frames = []
-                    break
-            frames.append(frame)
-        framework.ingest_data(None, frames, False)
-
-    # processing of a list of files contained in a file
-    elif args.file_list:
-        frames = []
-        with open(args.file_list) as file_list:
-            for frame in file_list:
-                if "#" not in frame:
-                    # Verify we have the correct channel selected
-                    if args.lowres and 'mr' in frame:
-                        print('	Low-res channel requested, but med-res files in list')
-                        qstr = input('Proceed? <cr>=yes or Q=quit: ')
-                        if 'Q' in qstr.upper():
-                            frames = []
-                            break
-                    if args.medres and 'lr' in frame:
-                        print('Med-res channel requested, but low-res files in list')
-                        qstr = input('Proceed? <cr>=yes or Q=quit: ')
-                        if 'Q' in qstr.upper():
-                            frames = []
-                            break
-                    frames.append(frame.strip('\n'))
-
-        framework.ingest_data(None, frames, False)
-
-        with open(args.file_list + '_ingest', 'w') as ingest_f:
-            ingest_f.write('Files ingested at: ' +
-                           datetime.datetime.now().isoformat())
-
     # ingest an entire directory, trigger "next_file" (which is an option
     # specified in the config file) on each file,
     # optionally continue to monitor if -m is specified
     elif args.dirname is not None:
 
         framework.ingest_data(args.dirname, None, args.monitor)
-
-    # implement the group mode
-    if args.group_mode is True:
-        data_set = framework.context.data_set
-
-        # remove focus images and ccd clearing images from the dataset
-        focus_frames = data_set.data_table[data_set.data_table.OBJECT ==
-                                           "focus"].index
-        ccdclear_frames = data_set.data_table[data_set.data_table.OBJECT ==
-                                              "Clearing ccd"].index
-        data_set.data_table.drop(focus_frames, inplace=True)
-        data_set.data_table.drop(ccdclear_frames, inplace=True)
-
-        # processing
-        imtypes = ['BIAS', 'CONTBARS', 'ARCLAMP', 'FLATLAMP', 'DOMEFLAT',
-                   'TWIFLAT', 'OBJECT']
-
-        for imtype in imtypes:
-            subset = data_set.data_table[
-                framework.context.data_set.data_table.IMTYPE == imtype]
-            if 'OBJECT' in imtype:  # Ensure that standards are processed first
-                object_order = []
-                standard_order = []
-                for frame in subset.index:
-                    if is_file_scales_std(frame, logger=framework.context.logger):
-                        standard_order.append(frame)
-                    else:
-                        object_order.append(frame)
-                order = standard_order + object_order  # Standards first
-                process_list(order)
-            else:
-                process_subset(subset)
 
     framework.config.instrument.wait_for_event = args.wait_for_event
     framework.config.instrument.continuous = args.continuous
